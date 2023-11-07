eee3151a71d184563bf06c6d34dc225fb65519d8
joey-cheng-codes <j.cheng93@gmail.com>
2023-11-06 22:28:22 -0500

    developing out skeleton app

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: path.resolve(__dirname, 'index.html')
+  })],
+  performance: {
+    hints: false
+  },
+  devServer: {
+    port: 8080,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:3000',
+      },
+    ],
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)$/,
+        loader: 'file-loader'
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
