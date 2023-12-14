565a5079528cc631bacafbf6ffd599712a02045f
joey-cheng-codes <j.cheng93@gmail.com>
2023-12-14 15:42:41 -0500

    fixed sizing of search bar and the modal dropdown of avatar

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -8,18 +8,26 @@ const Dashboard = (): React.JSX.Element => {
   return (
     <div className='dashboard-container'>
       <div className='logo-searchbar-container'>
-        <h1>ShareCycle</h1>
-        <SearchBar />
-        <button
-          data-modal-target="item-modal"
-          data-modal-toggle="item-modal"
-          className="mr-4"
-          type="button"
-          onClick={() => { setModalVisible((prevState) => !prevState); }}
-        >
-          <IoIosAddCircle />
-        </button>
-        <Avatar />
+        <div className='logo'>
+          <h1>ShareCycle</h1>
+        </div>
+        <div className='search-bar'>
+          <SearchBar />
+        </div>
+        <div className='add-item-button-container'>
+          <button
+            data-modal-target="item-modal"
+            data-modal-toggle="item-modal"
+            className="mr-4"
+            type="button"
+            onClick={() => { setModalVisible((prevState) => !prevState); }}
+          >
+            <IoIosAddCircle id='item-button' />
+          </button>
+        </div>
+        <div className='avatar-container'>
+          <Avatar />
+        </div>
       </div>
       {modalVisible && <Item setModalVisible={setModalVisible} />}
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   daisyui: {
@@ -9,6 +10,15 @@ module.exports = {
       require("daisyui"),
       require("@tailwindcss/forms"),
       require("flowbite/plugin"),
-    ]
+    ],
+  theme: {
+    extend: {
+      screens: {
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1280px",
+      },
+    },
+  }
 };
-
