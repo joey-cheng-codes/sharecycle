d8860f4ac554b38de95dec3d7be2640e90c21ada
joey-cheng-codes <j.cheng93@gmail.com>
2024-02-03 09:30:58 -0500

    refactored the login page using react-daisy-ui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+  ],
   daisyui: {
     themes: ["light"],
   },
@@ -11,14 +16,14 @@ module.exports = {
       require("@tailwindcss/forms"),
       require("flowbite/plugin"),
     ],
-  theme: {
-    extend: {
-      screens: {
-        "sm": "640px",
-        "md": "768px",
-        "lg": "1024px",
-        "xl": "1280px",
-      },
-    },
-  }
+  // theme: {
+  //   extend: {
+  //     // screens: {
+  //     //   "sm": "640px",
+  //     //   "md": "768px",
+  //     //   "lg": "1024px",
+  //     //   "xl": "1280px",
+  //     // },
+  //   },
+  // }
 };
